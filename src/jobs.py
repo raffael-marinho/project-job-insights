39e9a2058e3d526052f1df6d1230e27124c19347
raffael marinho <silvamarinho10@gmail.com>
2022-09-17 14:50:25 -0300

    feat: req 1 done

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,16 @@
+from email import header
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, encoding="utf-8") as file:
+        reader= csv.reader(file, delimeter=",")
+        header, *data=reader
+    docs = []
+    for line in data:
+        dictWithReader = {}
+        for row in range(len(header)):
+            dictWithReader[header[row]] = line[row]
+        docs.append(dictWithReader)
+    return docs
